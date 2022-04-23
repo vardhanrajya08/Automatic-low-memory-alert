dd0a257420950975b4e1041b8d231266cc227b4d
Rajya Vardhan <66107299+vardhanrajya08@users.noreply.github.com>
2022-04-24 02:43:26 +0530

    4th line error solved

--- alert.sh
+++ alert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TO="vardhanrajya08@gmail.com"        #add mail of system administrator
 ram_free=$(free -mt| grep Total:|awk '{print $4}')
-while:                             
+while :                             
 do
 
 if [ $ram_free -le 100 ]            # used bench mark as 100MB
